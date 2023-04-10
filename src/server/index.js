3095f874ae15e7a5d16fbff8b32defefd08dccd6
JavadJadidi <javadjadidi@gmail.com>
2023-04-10 18:55:09 +0430

    server-is-ready

--- src/server/index.js
+++ src/server/index.js
@@ -1,49 +1,49 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const port = 7777
-const token = 'javad-jadidi'
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const port = 7777;
+const token = "javad-jadidi";
 
-const app = express()
+const app = express();
 
-app.use(cors())
-app.use(bodyParser.json())
+app.use(cors());
+app.use(bodyParser.json());
 
-app.post('/login', (req, res) => {
-    const { username, password } = req.body
-    if ('admin' === username && 'admin' === password) {
-        res.send({
-            success: true,
-            data: token
-        })
-    }
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+  if ("admin" === username && "admin" === password) {
     res.send({
-        success: false,
-        error: 'username or password is wrong!'
-    })
-})
+      success: true,
+      data: token,
+    });
+  }
+  res.send({
+    success: false,
+    error: "username or password is wrong!",
+  });
+});
 
-app.get('/users/me', (req, res) => {
-    const { authorization } = req.headers
+app.get("/users/me", (req, res) => {
+  const { authorization } = req.headers;
 
-    if (token === authorization) {
-        res.send({
-            success: true,
-            data: {
-                id: 1,
-                username: 'admin',
-                email: 'info@im-javad.com',
-                name: 'admin'
-            }
-        })
-    }
-
+  if (token === authorization) {
     res.send({
-        success: false,
-        error: 'token is not valid'
-    })
-})
+      success: true,
+      data: {
+        id: 1,
+        username: "admin",
+        email: "info@gmail.com",
+        name: "admin",
+      },
+    });
+  }
+
+  res.send({
+    success: false,
+    error: "token is not valid",
+  });
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
